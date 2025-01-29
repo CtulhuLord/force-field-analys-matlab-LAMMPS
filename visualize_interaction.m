acee07ff935dc16460bfcf6892ffbf4dba38643b
CtulhuLord <mr.hulkbuster@gmail.com>
2025-01-29 17:44:25 +0300

    upd

--- visualize_interaction.m
+++ visualize_interaction.m
@@ -18,27 +18,9 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
         sizes = fixed_size * ones(size(interaction_array, 1), 1);
     end
 
-    % Отдельная нормализация для положительных и отрицательных значений
-    positive_values = interaction_array(:, 1) > 0;
-    negative_values = interaction_array(:, 1) < 0;
-    
-    % Нормализация положительных значений
-    positive_magnitudes = interaction_array(positive_values, 1);
-    positive_magnitudes_filtered = positive_magnitudes(positive_magnitudes <= threshold);
-    if ~isempty(positive_magnitudes_filtered)
-        normalized_positive = (positive_magnitudes - min(positive_magnitudes_filtered)) / (max(positive_magnitudes_filtered) - min(positive_magnitudes_filtered));
-    else
-        normalized_positive = [];
-    end
-
-    % Нормализация отрицательных значений
-    negative_magnitudes = abs(interaction_array(negative_values, 1));
-    negative_magnitudes_filtered = negative_magnitudes(negative_magnitudes <= threshold);
-    if ~isempty(negative_magnitudes_filtered)
-        normalized_negative = (negative_magnitudes - min(negative_magnitudes_filtered)) / (max(negative_magnitudes_filtered) - min(negative_magnitudes_filtered));
-    else
-        normalized_negative = [];
-    end
+    % Определение границ для нормализации с исключением экстремальных значений
+    critical_min = -threshold;
+    critical_max = threshold;
 
     % Создание массива цветов с использованием стандартных цветов MATLAB
     colors = zeros(size(interaction_array, 1), 3);
@@ -46,24 +28,28 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
     colormap_2 = colormap('jet');    % Используем стандартную colormap 'jet'
 
     % Установка цветов для положительных значений
+    positive_values = interaction_array(:, 1) > 0;
     positive_indices = find(positive_values);
     for i = 1:length(positive_indices)
         if interaction_array(positive_indices(i), 1) > threshold
             colors(positive_indices(i), :) = extreme_positive_color;
         else
-            color_index = round(normalized_positive(i) * (size(colormap_1, 1) - 1)) + 1;
+            normalized_positive = (interaction_array(positive_indices(i), 1) - critical_min) / (critical_max - critical_min);
+            color_index = round(normalized_positive * (size(colormap_1, 1) - 1)) + 1;
             color_index = max(1, min(color_index, size(colormap_1, 1))); % Проверка границ индекса
             colors(positive_indices(i), :) = colormap_1(color_index, :);
         end
     end
 
     % Установка цветов для отрицательных значений
+    negative_values = interaction_array(:, 1) < 0;
     negative_indices = find(negative_values);
     for i = 1:length(negative_indices)
         if abs(interaction_array(negative_indices(i), 1)) > threshold
             colors(negative_indices(i), :) = extreme_negative_color;
         else
-            color_index = round(normalized_negative(i) * (size(colormap_2, 1) - 1)) + 1;
+            normalized_negative = (interaction_array(negative_indices(i), 1) - critical_min) / (critical_max - critical_min);
+            color_index = round(normalized_negative * (size(colormap_2, 1) - 1)) + 1;
             color_index = max(1, min(color_index, size(colormap_2, 1))); % Проверка границ индекса
             colors(negative_indices(i), :) = colormap_2(color_index, :);
         end
@@ -78,6 +64,7 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
     zlabel('Z');
     title('Visualization of Interaction Magnitudes');
     colorbar;
+    caxis([critical_min, critical_max]);
     hold off;
 
     % Создание интерактивного окна для срезов
@@ -86,7 +73,7 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
     % Начальный размер шага
     initial_step = 1;
     h_slider = uicontrol(slice_fig, 'Style', 'slider', 'Min', min(interaction_array(:, 4)), 'Max', max(interaction_array(:, 4)), 'Value', min(interaction_array(:, 4)), 'Position', [400 20 120 20], 'SliderStep', [initial_step/(max(interaction_array(:, 4)) - min(interaction_array(:, 4))), initial_step/(max(interaction_array(:, 4)) - min(interaction_array(:, 4)))*10]);
-    addlistener(h_slider, 'Value', 'PostSet', @(src, event) updateSlice(slice_ax, interaction_array, event.AffectedObject.Value, colors));
+    addlistener(h_slider, 'Value', 'PostSet', @(src, event) updateSlice(slice_ax, interaction_array, event.AffectedObject.Value, critical_min, critical_max));
 
     % Текстовое поле для ввода шага
     h_step_text = uicontrol(slice_fig, 'Style', 'edit', 'String', num2str(initial_step), 'Position', [530 20 50 20]);
@@ -94,7 +81,7 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
     h_apply_button = uicontrol(slice_fig, 'Style', 'pushbutton', 'String', 'Apply Step', 'Position', [590 20 80 20], 'Callback', @(src, event) applyStep(h_slider, h_step_text, interaction_array));
 
     % Первоначальный вызов функции для отображения начального среза
-    updateSlice(slice_ax, interaction_array, min(interaction_array(:, 4)), colors);
+    updateSlice(slice_ax, interaction_array, min(interaction_array(:, 4)), critical_min, critical_max);
 
     % Создание окна для гистограммы, если включено show_histogram
     if show_histogram
@@ -109,7 +96,7 @@ function visualize_interaction(size_range, fixed_size, threshold, show_histogram
     fprintf('Визуализация завершена.\n');
 end
 
-function updateSlice(slice_ax, interaction_array, slice_value, colors)
+function updateSlice(slice_ax, interaction_array, slice_value, critical_min, critical_max)
     % Функция для обновления среза
     slice_index = round(slice_value);
     slice_data = interaction_array(abs(interaction_array(:, 4) - slice_index) < 1e-5, :); % Предполагается срез по координате Z
@@ -123,7 +110,10 @@ function updateSlice(slice_ax, interaction_array, slice_value, colors)
         [X, Y] = meshgrid(unique(slice_data(:, 2)), unique(slice_data(:, 3)));
         Z = griddata(slice_data(:, 2), slice_data(:, 3), slice_data(:, 4), X, Y);
         C = griddata(slice_data(:, 2), slice_data(:, 3), slice_data(:, 1), X, Y); % Используем значения для цвета
-        surf(slice_ax, X, Y, Z, C);
+        % Нормализация значений
+        C_norm = (C - critical_min) / (critical_max - critical_min);
+        surf(slice_ax, X, Y, Z, C_norm);
+        caxis([critical_min, critical_max]); % Установить те же пределы цветовой шкалы, что и для объёмной визуализации
         xlabel('X');
         ylabel('Y');
         zlabel('Z');
