bf58795a53a292edd1b8bafe7df4160eee7f97aa
CtulhuLord <mr.hulkbuster@gmail.com>
2025-01-29 16:41:00 +0300

    upd

--- main.m
+++ main.m
@@ -1,15 +1,26 @@
+% Ввод значения step
+step = input('Введите значение step: ');
 
-% Вызов основной функции
-sub();
+% Вызов основной функции с переданным значением step
+sub(step);
 
 % Основная функция для вызова вычислений и визуализации
-function sub()
+function sub(step)
     % Проверка существования файла interaction_array.mat
-    if exist('interaction_array.mat', 'file') ~= 2
-        % Выполнение вычислений и сохранение массива interaction_array в файл
-        compute_interaction();
+    if exist('interaction_array.mat', 'file') == 2
+        % Загрузка сохраненного значения step из файла interaction_array.mat
+        saved_data = load('interaction_array.mat');
+        
+        if isfield(saved_data, 'step') && saved_data.step == step
+            fprintf('Файл interaction_array.mat уже существует и step совпадает. Вычисления пропущены.\n');
+        else
+            fprintf('Файл interaction_array.mat существует, но step отличается. Пересчет взаимодействий...\n');
+            % Выполнение вычислений и сохранение массива interaction_array в файл
+            compute_interaction(step);
+        end
     else
-        fprintf('Файл interaction_array.mat уже существует. Вычисления пропущены.\n');
+        % Выполнение вычислений и сохранение массива interaction_array в файл
+        compute_interaction(step);
     end
 
     % Параметры для настройки размеров точек
